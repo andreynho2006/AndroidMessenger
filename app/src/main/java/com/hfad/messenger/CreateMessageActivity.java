8627d90c46bcbeac1213c18753a1a8581c27616d
andreynho2006 <andreynho2006@gmail.com>
2018-12-03 23:34:57 +0200

    make 2 acttivities, use Intent.ACTION_SEND to send a message

--- app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
+++ app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends Activity {
 
@@ -15,7 +16,14 @@ public class CreateMessageActivity extends Activity {
 
     // call onSendMessage() when the button is clicked
     public void onSendMessage(View view) {
-        Intent intent = new Intent(this, ReceiveMessageActivity.class);
-        startActivity(intent);
+        EditText messageView = (EditText)findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+        String chooserTitle = getString(R.string.chooser);
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+        startActivity(chosenIntent);
     }
 }
